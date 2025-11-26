7007a397510a7f216c7d826bceecbda7329de3fc
YishayOssi <yishayossi10@gmail.com>
2025-11-26 16:36:29 +0200

    commit number 2

---
+++ DataBaseManager.py
@@ -0,0 +1,43 @@
+import sqlite3
+from classes import Soldier
+
+class DatabaseManageri:
+    def __init__(self, db_name="soldier.db"):
+        self.db_name = db_name
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS table_of_soldier (
+                id INTEGER PRIMARY KEY UNIQUE,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL,
+                gender TEXT NOT NULL,                       
+                city INTEGER NOT NULL,
+                Distance_from_base INTEGER NOT NULL
+            )
+        """)
+        self.connection.commit()
+
+    def insert_soldier(self, soldieri):
+     self.cursor.execute(
+        "INSERT INTO table_of_soldier (id, first_name, last_name, gender, city, Distance_from_base) VALUES (?, ?, ?, ?, ?, ?)", (soldieri.id, soldieri.first_name, soldieri.last_name, soldieri.gender, soldieri.city, soldieri.Distance_from_base))
+     
+     self.connection.commit()
+
+
+
+    def get_all_persons(self):
+     self.cursor.execute("SELECT * FROM table_of_soldier")
+     rows = self.cursor.fetchall()
+
+     persons = []
+     for row in rows:
+        person = persons(row[0], row[1], row[2], row[3], row[4], row[5])
+        persons.append(person)
+
+     return persons
+    
+
+
